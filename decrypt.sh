91ee6089a7e69d3def2afd6464acd61d4077c59f
Ivan Korhner <korhner@gmail.com>
2021-11-26 22:30:15 +0100

    add password prompt in recover.sh

--- decrypt.sh
+++ decrypt.sh
@@ -2,5 +2,12 @@
 
 set -e
 
+if [ -z "$PASSWORD" ]
+then
+  echo "Insert password for LUKS disk encryption (you will not see it)"
+  read -s PASSWORD
+  export PASSWORD
+fi
+
 echo -n "$PASSWORD" | cryptsetup open /dev/disk/by-partlabel/cryptsystem system -d -
 cryptsetup open /dev/disk/by-label/cryptswap swap --key-file=/dev/urandom --offset=1024 --type=plain --cipher=aes-xts-plain64:sha256
