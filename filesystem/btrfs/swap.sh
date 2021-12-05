b7c1b5d10ce807ef244cbcf6238138daacbbca7e
Ivan Korhner <korhner@gmail.com>
2021-12-05 12:29:40 +0100

    fix swap path

--- filesystem/btrfs/swap.sh
+++ filesystem/btrfs/swap.sh
@@ -5,5 +5,5 @@ chmod 600 /mnt/swap/swapfile
 chattr +C /mnt/swap/swapfile
 dd if=/dev/zero of=/mnt/swap/swapfile bs=1M count="$SWAP_PARTITION_SIZE_MB"
 
-arch-chroot /mnt mkswap /mnt/swap/swapfile
-arch-chroot /mnt swapon /mnt/swap/swapfile
+arch-chroot /mnt mkswap /swap/swapfile
+arch-chroot /mnt swapon /swap/swapfile
