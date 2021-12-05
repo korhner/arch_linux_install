2295c62f416c8832348387ecb020e12c31e9d3f2
Ivan Korhner <korhner@gmail.com>
2021-12-05 11:57:12 +0100

    try to fix swap mount

--- filesystem/btrfs/format.sh
+++ filesystem/btrfs/format.sh
@@ -17,12 +17,13 @@ btrfs subvolume create /mnt/@swap
 umount -R /mnt
 
 echo "Create swap file"
-mount -o subvol=@swap,X-mount.mkdir LABEL=system /mnt/swap
+mount -t btrfs -o subvol=@swap,X-mount.mkdir LABEL=system /mnt/swap
 touch /mnt/swap/swapfile
 chmod 600 /mnt/swap/swapfile
 chattr +C /mnt/swap/swapfile
 dd if=/dev/zero of=/mnt/swap/swapfile bs=1M count="$SWAP_PARTITION_SIZE_MB"
-mkswap /mnt/swap/swapfile
-swapon /mnt/swap/swapfile
+
+arch-chroot /mnt mkswap /mnt/swap/swapfile
+arch-chroot /mnt swapon /mnt/swap/swapfile
 #UUID=XXXXXXXXXXXXXXX /swap btrfs subvol=@swap 0 0
 #/swap/swapfile none swap sw 0 0
